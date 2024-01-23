1d6d58fe4beb1502335531763c3ddd5a17476777
Tung Dang <tungds@vimo.vn>
2024-01-23 15:00:05 +0700

    update

--- genpassword.py
+++ genpassword.py
@@ -1,8 +1,23 @@
 import string
 import random
+LETTERS = string.ascii_letters
+NUMBERS = string.digits
+PUNCTUATION = string.punctuation
+
+
+def gen_password(length = 8):
+    printable = f'{LETTERS}{NUMBERS}{PUNCTUATION}'
+    printable = list(printable)
+    random.shuffle(printable)
+    randomPass = random.choices(printable, k=length)
+    randomPass = "".join(randomPass)
+    return randomPass
 
 def main():
-    pass
+    lenthPass = int(input("How long do you want your password: "))
+    genPass = gen_password(lenthPass)
+    print(genPass)
+
 
 if __name__ == '__main__':
     main()
