c6e52eb49b7977d9102fd2545fc171f4d2de7d0e
Tung Dang <tungds@vimo.vn>
2024-01-24 19:11:15 +0700

    update

--- excercise/sangsonguyento.py
+++ excercise/sangsonguyento.py
@@ -1,6 +1,13 @@
 ## sang so nguyen to:
+def isMersenne(x):
+    x += 1
+    i = 1
 
-n = 1000
+    while 2 ** i < x:
+        i+= 1
+    return x == 2 ** i
+
+n = 10000
 numList = [True] * n
 soNt = []
 
@@ -14,4 +21,18 @@ for i in range(2, n):
     if numList[i] == True:
         soNt.append(i)
 
-print(soNt)
+
+for i in range(2, n):
+    if numList[i] == True and isMersenne(i) == True:
+        print(i)
+
+### tim so nguyen to lan nhat co n chu so:
+n = int(input("Nhap vao so chu so : "))
+max = int('9'* n)
+
+while max not in soNt:
+    max-= 1
+print(max)
+
+
+
