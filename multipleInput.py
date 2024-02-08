f1ce29ed971f643747a922b6b787ed5e97136ac6
Tung Dang <tungds@vimo.vn>
2024-02-08 15:16:45 +0700

    update

---
+++ multipleInput.py
@@ -0,0 +1,26 @@
+firstname, lastname, email = input("Enter your detail, seperate bay commas (,) : ").split(",")
+print(firstname.strip(), lastname.strip(), email.strip())
+
+# a, b, c = input("Nhap vao 3 so nguyen:   ").split()
+# print(a, b, c)
+
+### convert to integer :
+# d, e, f = map(int, input("Enter 3 interger number : ").split())
+# print(type(d), type(e), type(f))
+
+
+# list_a = [s for s in input("Nhap item : ").split(",")]
+# print(list_a)
+
+# list_a = list(map(int, input().split(",")))
+# print(list_a)
+
+# list_b = [int(s) for s in input("Nhap item: ").split(",")]
+# print(list_b)
+
+# list_a = [int(input()) for _ in range(6)] ### input in multiple line
+# print(list_a)
+
+# list_word = [input() for _ in range(int(input("Nhap vao so tu :  ")))]
+# print(list_word)
+
